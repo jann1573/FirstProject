609bbc68880e934691bda390a74869a93e98cc83
Raihana-2964 <zainabraihana1985@gmail.com>
2024-03-30 14:09:22 +0530

    changes

--- mul.java
+++ mul.java
@@ -1,11 +1,11 @@
 public class MultiplyExample {
    public static void main(String[] args) {
-       float a;
+       double a;
        int b;
        int c;
        a = 5;
        b = 58;
-       c = a+b; //integer number to keep the result of multiplication
+       c = a*b; //integer number to keep the result of multiplication
        System.out.println("5*58 = " + c);
    }
 }
